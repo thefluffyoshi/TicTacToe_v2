a170db8d404b52cc748ae6b3cf1bb9a66f0302f6
thefluffyoshi <54290315+thefluffyoshi@users.noreply.github.com>
2020-11-15 23:04:04 -0800

    added onClickPlayAgain

--- app/src/main/java/com/example/tictactoev1/MainActivity3.java
+++ app/src/main/java/com/example/tictactoev1/MainActivity3.java
@@ -2,7 +2,9 @@ package com.example.tictactoev1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+    }
+
+    public void onClickPlayAgain(View view)
+    {
+        startActivity(new Intent(MainActivity3.this, MainActivity.class));
     }
 }
