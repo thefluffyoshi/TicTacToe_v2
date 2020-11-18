8f86c75fb06527fe4bee170688b78586bdb25992
chewbecca615 <rchang615@gmail.com>
2020-11-18 15:14:09 -0800

    finished basic tictactoe

--- app/src/main/java/com/example/tictactoev1/MainActivity.java
+++ app/src/main/java/com/example/tictactoev1/MainActivity.java
@@ -2,19 +2,124 @@ package com.example.tictactoev1;
 
 import android.content.Intent;
 import android.os.Bundle;
+<<<<<<< Updated upstream
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+=======
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button[][] buttons = new Button[3][3];
+    private boolean playerOneTurn = true;
+    private int roundCount;
+>>>>>>> Stashed changes
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+<<<<<<< Updated upstream
     }
 
     public void onClickNextPage(View view)
     {
         startActivity(new Intent(MainActivity.this, MainActivity2.class));
+=======
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                String buttonID = "button_" + i + j;
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                buttons[i][j] = findViewById(resID);
+                buttons[i][j].setOnClickListener(this);
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (!((Button) view).getText().toString().equals("")) {
+            return;
+        }
+        if (playerOneTurn) {
+            ((Button) view).setText("X");
+        } else {
+            ((Button) view).setText("O");
+        }
+        roundCount++;
+        if (checkForWin()) {
+            if (playerOneTurn) {
+                playerOneWins();
+            } else {
+                playerTwoWins();
+            }
+        } else if (roundCount == 9) {
+            draw();
+        } else {
+            playerOneTurn = !playerOneTurn;
+        }
+    }
+
+    private boolean checkForWin() {
+        String[][] field = new String[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                field[i][j] = buttons[i][j].getText().toString();
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            if (field[i][0].equals(field[i][1])
+                    && field[i][0].equals(field[i][2])
+                    && !field[i][0].equals("")) {
+                return true;
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            if (field[0][i].equals(field[1][i])
+                    && field[0][i].equals(field[2][i])
+                    && !field[0][i].equals("")) {
+                return true;
+            }
+        }
+        if (field[0][0].equals(field[1][1])
+                && field[0][0].equals(field[2][2])
+                && !field[0][0].equals("")) {
+            return true;
+        }
+        if (field[0][2].equals(field[1][1])
+                && field[0][2].equals(field[2][0])
+                && !field[0][2].equals("")) {
+            return true;
+        }
+        return false;
+    }
+
+    private void playerOneWins() {
+        // takes to screen where it says player one wo
+        resetBoard();
+    }
+    private void playerTwoWins() {
+        // takes to screen where it says player one won
+        resetBoard();
+    }
+    private void draw() {
+        // takes to screen where it says player one won
+        resetBoard();
+    }
+    private void resetBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+            }
+        }
+        roundCount = 0;
+        playerOneTurn = true;
+>>>>>>> Stashed changes
     }
 }
