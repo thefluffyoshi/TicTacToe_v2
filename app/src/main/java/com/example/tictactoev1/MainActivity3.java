19a28899f9779d3a98cdaafa2d569ed9c45fa255
thefluffyoshi <54290315+thefluffyoshi@users.noreply.github.com>
2020-11-20 18:56:48 -0800

    attempted to change TV from another class

--- app/src/main/java/com/example/tictactoev1/MainActivity3.java
+++ app/src/main/java/com/example/tictactoev1/MainActivity3.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -12,10 +13,13 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+//        TextView winner = (TextView) findViewById(R.id.winnerTV);
+//        winner.setText("X wins");
     }
 
     public void onClickPlayAgain(View view)
     {
-        startActivity(new Intent(MainActivity3.this, MainActivity.class));
+        startActivity(new Intent(MainActivity3.this, MainActivity2.class));
     }
 }
